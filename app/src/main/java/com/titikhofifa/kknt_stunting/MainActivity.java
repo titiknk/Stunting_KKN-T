be24f5f35a281cc20e603e79c15533f552830770
titiknk <titikhofifa@gmail.com>
2023-03-18 21:39:48 +0700

    MainActivity

--- app/src/main/java/com/titikhofifa/kknt_stunting/MainActivity.java
+++ app/src/main/java/com/titikhofifa/kknt_stunting/MainActivity.java
@@ -1,8 +1,12 @@
 package com.titikhofifa.kknt_stunting;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.btnStart);
+
+        button1.setOnClickListener(view -> openLandingActivity());
+    }
+
+    private void openLandingActivity() {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setIcon(R.drawable.ic_baseline_exit_to_app_24);
+        alertDialog.setTitle("Keluar dari Aplikasi");
+        alertDialog.setMessage("Apakah kamu ingin keluar dari Aplikasi?");
+        alertDialog.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+            }
+        });
+        alertDialog.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 }
