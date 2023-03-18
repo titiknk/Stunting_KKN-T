816caf895720851e666baa948073779f784751d6
titiknk <titikhofifa@gmail.com>
2023-03-18 11:54:37 +0700

    Aplikasi Edukasi Pencegahan Stunting

---
+++ app/src/main/java/com/titikhofifa/kknt_stunting/splashscreen.java
@@ -0,0 +1,35 @@
+package com.titikhofifa.kknt_stunting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(decorView.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        if (getSupportActionBar() != null) {
+            getActionBar().hide();
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    intent = new Intent(splashscreen.this, MainActivity.class);
+                }
+                startActivity(intent);
+                finish();
+            }
+        }, 1850);
+    }
+}
